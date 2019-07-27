8c5114d8668fb41cafe85d59c8b67640b25a1646
AUGUSTO <augustohtp8@gmail.com>
2019-07-27 03:54:00 -0300

    TrucksController

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::middleware('auth:api')->group(function () {
 
     Route::prefix('truck')->group(function () {
         Route::put('/up/{id}', 'TrucksControllers@updateTruck')->where('id', '[0-9]+');
-        Route::delete('/del/{id}', 'TrucksController@deleteTruck')->where('id', '[0-9]+');
+        Route::delete('/del/{id}', 'TrucksControllers@deleteTruck')->where('id', '[0-9]+');
     });
     Route::prefix('freight')->group(function() {
         Route::get('/user', 'FreightsController@findAllbyUser');
